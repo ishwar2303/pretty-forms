c00cef735ee1a47dd5a93aa93c5d366bab99b10b
Ishwar Baisla <ishwar2303@gmail.com>
2021-01-18 03:41:26 +0530

    v 0.3

--- header-scoll.js
+++ header-scoll.js
@@ -1,7 +1,5 @@
 
-window.onscroll = function() {myFunction()};
-
-function myFunction() {
+function setHeaderOnScroll1() {
 
     var header = document.getElementById("pretty-forms-header")
     var backToTop = document.getElementById('back-to-top')
@@ -15,3 +13,28 @@ function myFunction() {
         backToTop.style.display = 'none'
     }
 }
+function setHeaderOnScroll2() {
+
+    var header = document.getElementById("pretty-forms-header")
+    var backToTop = document.getElementById('back-to-top')
+    let pageYoffset = window.pageYOffset
+    if(pageYoffset > 160){
+        header.className = 'sticky'
+        backToTop.style.display = 'block'
+    }
+    else{
+        header.className = 'non-sticky'
+        backToTop.style.display = 'none'
+    }
+}
+
+var targetWidth = 600;
+$(document).ready(() => {
+    if ( $(window).width() > targetWidth) {   
+        window.onscroll = function() {setHeaderOnScroll1()};
+    }
+    else {
+        window.onscroll = function() {setHeaderOnScroll2()};
+    }
+
+})
